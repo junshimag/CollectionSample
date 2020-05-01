e553622d21093d1af93d6d82998a27994e23b4d8
junya shimaguchi <shimaguchi@junyanoMacBook-Pro.local>
2020-05-02 07:07:41 +0900

    first commit

---
+++ CollectionSample/TestLabelCollectionViewItem.swift
@@ -0,0 +1,20 @@
+//
+//  TestLabelCollectionViewItem.swift
+//  CollectionSample
+//
+//  Created by shimaguchi on 2020/05/02.
+//  Copyright © 2020 junshimag. All rights reserved.
+//
+
+import Cocoa
+
+class TestLabelCollectionViewItem: NSCollectionViewItem {
+
+    @IBOutlet var testLabel:NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
